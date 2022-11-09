99570d11071d0a8362e98ccff1a2fef89e848fa7
Hifumi1337 <56496067+Hifumi1337@users.noreply.github.com>
2022-11-09 08:40:13 -0600

    v0.0.5
    - Lucy is finally stable
    - All values in Mercy are now available in the CLI

--- README.md
+++ README.md
@@ -1,23 +1,61 @@
-# Lucy
-Lucy is a simple command line interface built on top of the Mercy Rust crate. Lucy takes the entirety of Mercy and converts it into an easy to use CLI for quick security assessments and demonstrates the custom crate.
+<h1 align="center">
+    <img src="assets/lucy_logo.png" />
+    <br />
+    Lucy
+</h1>
+
+Lucy is a simple command line interface built on top of the [Mercy](https://github.com/umikolabs/mercy) Rust crate. Lucy takes the entirety of Mercy and converts it into an easy to use CLI for quick security assessments and demonstrates the crate's abilities.
 
 ### Usage
+
+While Lucy is in an alpha state, the source will need to be built. You can easily build the source using the cargo package manager like so:
+
 ```bash
 cargo build
 ```
 
+Once the executable has been compiled, you can run the CLI tool using the following syntax:
 ```bash
-./lucy -m <METHOD> -p <PROTOCOL> -i <STRING>
+./lucy -m <METHOD> -p <PROTOCOL> -i <STRING/FILE>
 ```
 
-You can also run the help command:
-```
+You can also run the help command if you need a refresher on the available arguments:
+```bash
 ./lucy -h
 ```
 
+We've collected the available commands and listed them below:
+```
+-i, --input     Encoded/Plaintext string for decoding/encoding (ex: IaMStr1Ng) + location of the file for hex_dump
+
+-m, --method    Chosen method for data manipulation (ex: decode)
+
+-p, --protocol  Chosen protocol for data manipulation (ex: base64)
+
+-e, --extended  View every available option within the Lucy CLI
+```
+
 ### Examples
+
+Here are some quick examples for the `decode`, `sys`, `hash`, and `ip` arguments:
+
 ```bash
 ./lucy -m decode -p base64 -i <EncodedString>
-./lucy -m encode -p base64 -i <PlaintextString>
+```
+
+```bash
+./lucy -m sys -p system_info -i all
+```
+
+```bash
 ./lucy -m hash -p md5 -i <PlaintextString>
+```
+
+```bash
+./lucy -m ip -p internal_ip
+```
+
+If you're still stuck, you can use this option to learn every command at your disposal:
+```bash
+./lucy -e
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,109 @@
+/*
+    Umiko Labs (https://github.com/umikolabs)
+    File: src/main.rs
+
+    Author(s): {
+        Hifumi1337 (https://github.com/Hifumi1337)
+    }
+*/
+
 use mercy;
 use clap::Parser;
 
-// TODO: Add hex_dump functionality from Mercy
+use prettytable::{
+    Cell,
+    Row,
+    Table
+};
 
 #[derive(Parser, Debug)]
 #[command(name = "Lucy")]
-#[command(version, author)] // Reads from Cargo.toml
+#[command(version)] // Reads from Cargo.toml
 #[command(about = "Lucy is a simple command line interface built on top of the Mercy Rust crate", long_about = None)]
 struct Args {
 
-    /// Encoded/Plaintext string for decoding/encoding (ex: IaMStr1Ng)
+    /// Encoded/Plaintext string for decoding/encoding (ex: IaMStr1Ng) + location of the file for hex_dump
     #[arg(short, long)]
+    #[clap(default_value = "")]
     input: String,
 
     /// Chosen method for data manipulation (ex: decode)
     #[arg(short, long)]
+    #[clap(default_value = "")]
     method: String,
 
     /// Chosen protocol for data manipulation (ex: base64)
     #[arg(short, long)]
-    protocol: String
+    #[clap(default_value = "")]
+    protocol: String,
+
+    /// View every available option within the Lucy CLI
+    #[arg(short, long)]
+    extended: bool
+}
+
+fn pretty_output(input: &str, output: &str, left_col: &str, right_col: &str) {
+    let mut table = Table::new();
+
+    table.add_row(Row::new(vec![
+        Cell::new(left_col),
+        Cell::new(right_col)
+    ]));
+
+    table.add_row(Row::new(vec![
+        Cell::new(input),
+        Cell::new(output)
+    ]));
+
+    table.printstd();
 }
 
 fn main() {
     let args = Args::parse();
 
-    match args.method.as_str() {
+    if args.extended {
+        println!("\n=== Lucy ===");
+        pretty_output("encode\ndecode\nhash\nhex\nsys\nip", "base64, rot13\nbase64, rot13\nmd5, sha2_256\nhex_dump\nsystem_info\ninternal_ip", "Method(s)", "Protocol(s)");
 
-        "decode" => {
-            println!("{}",mercy::mercy_decode(&args.protocol, &args.input));
-        },
+        println!("\n=== Lucy Extended ===");
+        pretty_output("system_info", "hostname\ncpu_cores\ncpu_speed\nos_release\nproc\nall", "Protocol(s)", "Input(s)");
 
-        "encode" => {
-            println!("{}", mercy::mercy_encode(&args.protocol, &args.input));
-        },
+        println!("\n=== Examples ===");
+        println!("Print general information for the local system");
+        println!("./lucy -m sys -p system_info -i all\n");
 
-        "hash" => {
-            println!("{}", mercy::mercy_hash(&args.protocol, &args.input));
-        },
+        println!("Decode an encoded string using base64");
+        println!("./lucy -m decode -p base64 -i dW1pa28gbGFicyBpcyB0aGUgYmVzdCB3YWlmdQ==\n");
+    } else {
+        match args.method.as_str() {
 
-        "sys" => {
-            println!("{}", mercy::mercy_extra(&args.protocol, "all"));
-            println!("{}", mercy::mercy_extra("internal_ip", ""));
-        },
-
-        _ => {
-            println!("Unable to parse provided arguments. Please check inputs");
+            "decode" => {
+                println!("{}",mercy::mercy_decode(&args.protocol, &args.input));
+            },
+    
+            "encode" => {
+                println!("{}", mercy::mercy_encode(&args.protocol, &args.input));
+            },
+    
+            "hash" => {
+                println!("{}", mercy::mercy_hash(&args.protocol, &args.input));
+            },
+    
+            "hex" => {
+                println!("{}", mercy::mercy_hex(&args.protocol, &args.input));
+            },
+    
+            "sys" => {
+                println!("{}", mercy::mercy_extra(&args.protocol, &args.input));
+            },
+    
+            "ip" => {
+                println!("{}", mercy::mercy_extra(&args.protocol, &args.input));
+            },
+    
+            _ => {
+                println!("Unable to parse provided arguments");
+            }
         }
     }
 }
