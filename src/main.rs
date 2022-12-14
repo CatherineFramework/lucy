fb02aac185958dcef81febe971794f4b0abc23ec
Hifumi1337 <56496067+Hifumi1337@users.noreply.github.com>
2022-12-14 16:51:14 -0600

    Minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 /*
-    CyberSuki (https://github.com/cybersuki)
-    File: src/main.rs
-
-    Author(s): {
-        Hifumi1337 (https://github.com/Hifumi1337)
-    }
+    Owner: Hifumi1337 (https://github.com/hifumi1337)
+    Project: Lucy
+    License: BSD 2-Clause
 */
 
 use mercy;
